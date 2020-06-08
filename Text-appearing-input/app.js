1d1e2493bd9a942f25c7ac56abd8c4c36aeb16c3
Adrian Pruszkowski <pruszkowski.adrian@gmail.com>
2020-06-08 06:55:50 +0100

    Add files via upload

--- Text-appearing-input/app.js
+++ Text-appearing-input/app.js
@@ -3,25 +3,31 @@ class App extends React.Component {
         value: ""
 
     }
-    handleInputChange = (e) => {
-        console.log(e.target.value)
+    handleChange = (e) => {
+        console.log("Inside event" + e.target.value)
+        console.log("value before setState " + this.state.value)
+
         this.setState({
             value: e.target.value
         })
+        console.log("value after setState " + this.state.value)
+
     }
 
-    handleResetClick = () => {
+    handleClick = () => {
         this.setState({
             value: ""
         })
     }
 
     render() {
+        console.log("value during render " + this.state.value)
+
         return (
             <>
                 <input value={this.state.value} placeholder="Write text"
-                    onChange={this.handleInputChange} type="text" />
-                <button onClick={this.handleResetClick} >Reset</button>
+                    onChange={this.handleChange} type="text" />
+                <button onClick={this.handleClick} >Reset</button>
                 <h1 className="title">{this.state.value.toUpperCase()}</h1>
             </>
         )
